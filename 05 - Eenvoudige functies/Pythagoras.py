b3913a8648f1a5bb8e7882e634422256910fe11f
florencevandendorpe <florence.vandendorpe@sgsintpaulus.eu>
2018-10-09 17:48:54 +0200

    Pythagoras opgelost

--- 05 - Eenvoudige functies/Pythagoras.py
+++ 05 - Eenvoudige functies/Pythagoras.py
@@ -2,4 +2,11 @@
 a = float(input('a: '))
 b = float(input('b: '))
 
-a ** 2 + b ** 2 = c ** 2
+#berekening
+from math import sqrt
+c = sqrt( a ** 2 + b ** 2)
+
+
+
+#schuine zijde berekenen
+print('In een rechthoekige driehoek met rechthoekszijden a = ' + ('{:.2f}'.format(a)) +  ' en b = ' + ('{:.2f}'.format(b)) + ' is de schuine zijde ' + ('{:.2f}'.format(c)))
