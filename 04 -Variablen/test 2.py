be647ae55110012b2c029a5cd9d07dcd74f66b75
florencevandendorpe <florence.vandendorpe@sgsintpaulus.eu>
2018-10-11 08:30:19 +0200

    test2

---
+++ 04 -Variablen/test 2.py
@@ -0,0 +1,9 @@
+aantal_leerlingen_in_klas = 18
+
+nieuwe_leerlingen = int(input( 'aantal nieuwe leerlingen : '))
+
+#aantal_leerlinen_in_klas = aantal_leerlingen_in_klas = nieuwe_leerlingen
+aantal_leerlinge_in_klas = aantal_leerlingen_in_klas + nieuwe_leerlingen
+aantal_leerlinge_in_klas *= nieuwe_leerlingen
+
+print(aantal_leerlinge_in_klas)
