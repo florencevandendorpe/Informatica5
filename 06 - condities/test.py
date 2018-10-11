11e90304f65fb598e8fe8787f246a60a867f6f17
florencevandendorpe <florence.vandendorpe@sgsintpaulus.eu>
2018-10-11 09:19:15 +0200

    test

---
+++ 06 - condities/test.py
@@ -0,0 +1,2 @@
+lengte_w > 1.75 or lengte_f > 1.75 goed alles voluit schrijven
+lengte_w and lengte_f > 1.75 fout
---
+++ 06 - condities/test2.py
@@ -0,0 +1,9 @@
+x = int(input('Geef een geheel getal: '))
+
+if x % 2 == 0:
+    #commentaar
+    print(x, 'is een even getal')
+else:
+    print(x, 'is een oneven getal')
+
+print('tot ziens!')
