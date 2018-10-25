62c9b2e40458bca3d5e318f5833f54a0e9464507
florencevandendorpe <florence.vandendorpe@sgsintpaulus.eu>
2018-10-25 09:57:01 +0200

    irrationale functie opgelost

--- toets/irrationale functie.py
+++ toets/irrationale functie.py
@@ -1,12 +1,13 @@
 #invoer
 a = float(input('geef waarde van x: '))
-a
+
+#formule
 from math import sqrt
-b = sqrt(a) - sqrt(2)
+b = sqrt(abs(((a) - 2)))
 
 #berekening
 if a < 2:
-    print('{:.2f}'.format(a) + ' NOT AN ELEMENT OF dom(f)')
+    print('{:.2f}'.format(a) + ' ∉ dom(f)')
 elif a == 2:
     print('{:.2f}'.format(a) + ' is nulpunt van f')
 else:
