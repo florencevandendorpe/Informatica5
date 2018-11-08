0456bef201b6e9bcd98c9249d417afb4ef3f3e33
florencevandendorpe <florence.vandendorpe@sgsintpaulus.eu>
2018-11-08 09:41:15 +0100

    omkeren opgelost

---
+++ 07a - Iteraties - For-lus/omkeren.py
@@ -0,0 +1,12 @@
+#invoer
+woord = input('woord: ')
+nieuw_woord = ''
+
+for letter in woord:
+    nieuw_woord = letter + nieuw_woord
+
+
+#uitvoer
+print(nieuw_woord)
+
+
---
+++ 07a - Iteraties - For-lus/veelvouden.py
@@ -0,0 +1,5 @@
+#input
+r = float(input('geef getal: '))
+
+for i in range(1, 100):
+    cijfers += str(r * i)
