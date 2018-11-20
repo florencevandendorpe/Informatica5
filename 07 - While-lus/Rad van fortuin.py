dbc1b2e09edced05838343b85bc1dc16981f3c9d
florencevandendorpe <florence.vandendorpe@sgsintpaulus.eu>
2018-11-20 17:11:30 +0100

    rad van fortuin opgelost

--- 07 - While-lus/Rad van fortuin.py
+++ 07 - While-lus/Rad van fortuin.py
@@ -1,12 +1,16 @@
 #invoer
-woord = input('geef verborgen woord: ')
+woord = str(input('geef verborgen woord: '))
 bedrag = int(input('geef bedrag: '))
-letters = input('geef letters: ')
+letters = str(input('geef letters: '))
 gewonnen_bedrag = 0
-
+x = ''
 #berekening
-while letters in woord:
-    letters = input('geef letters: ')
-    gewonnen_bedrag = gewonnen_bedrag + bedrag
-if letters not in woord:
+while letters in woord and letters not in x:
+    gewonnen_bedrag += bedrag
+    x += letters
+    letters = str(input('geef letters: '))
+
+if letters not in woord or letters in x:
     print(gewonnen_bedrag)
+
+
