b45ccfee603847395ff39b560402366f49bd159a
florencevandendorpe <florence.vandendorpe@sgsintpaulus.eu>
2018-11-20 18:35:40 +0100

    priemgetalen opgelost

--- 07 - While-lus/priemgetallen.py
+++ 07 - While-lus/priemgetallen.py
@@ -1,6 +1,18 @@
 #invoer
 getal = int(input('geef een getal :'))
+a = 2
+b = 1
 
-while getal / 1 and getal / getal = 1:
+#berekening
+while a < getal:
+    if (getal % a) == 0:
+        resultaat = '{} is geen priemgetal'.format(getal)
+        b = 0
+    a += 1
+if b and getal != 1:
+    resultaat = '{} is een priemgetal'.format(getal)
+elif getal == 1:
+    resultaat  = '1 is geen priemgetal'
 
-    print( getal, 'is een priemgetal')
+#uitvoer
+print(resultaat)
